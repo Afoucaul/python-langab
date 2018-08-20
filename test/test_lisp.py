201b0efcc6e148cac0864c69e96659577f06369b
armand <armand.foucault@telecom-bretagne.eu>
2018-08-20 23:59:51 +0200

    Added a dummy test

--- test/test_lisp.py
+++ test/test_lisp.py
@@ -5,3 +5,6 @@ from langab.lisp import let
 class TestLisp(unittest.TestCase):
     def test_let(self):
         self.assertEqual((let (a = 1, b = 2) (a + b)), 3)
+
+    def test_let_2(self):
+        self.assertEqual((let (c = 12) (2 * c)), 24)
